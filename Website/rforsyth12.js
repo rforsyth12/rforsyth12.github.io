5b3718d48a8f14e18192231f2b727f0befd46cf4
Ryan Forsyth <ryanforsyth@Ryans-MBP.fios-router.home>
2017-01-08 13:51:59 -0500

    Website improvement
    Added CSS and JS files.

---
+++ Website/rforsyth12.js
@@ -0,0 +1,6 @@
+function header () {
+    document.writeln("<h1>Ryan Forsyth</h1>")
+    document.writeln("<div>");
+    document.writeln("<a href='index.html'>Home Page</a>");
+    document.writeln("</div>");
+}
